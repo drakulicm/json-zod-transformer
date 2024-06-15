4dca208ae40dc7661d5b2dccf3f787ebbd829ed6
Marko <drakulic.marko0@gmail.com>
2024-06-15 15:12:25 +0200

    feat: Update eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image"
-
 const Home = () => {
-  return <main className="era p-4 pl-2">Hello world</main>
+  return <main className="p-4 pl-2">Hello world</main>
 }
 
 export default Home
