1cc6816d51fc50c436a89e165c1fe0115b9cd379
Marko <drakulic.marko0@gmail.com>
2024-09-08 12:58:34 +0200

    feat: JSON camel case convert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,42 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import camelcaseKeysDeep from "camelcase-keys-deep"
+
 import { Textarea } from "@/components/ui/textarea"
 
 const Home = () => {
+  const [input, setInput] = useState("")
+  const [output, setOutput] = useState("")
+
+  const camelCaseJson = (json: string) => {
+    try {
+      const parsedJson = JSON.parse(json)
+      const camelCaseJson = camelcaseKeysDeep(parsedJson)
+      return JSON.stringify(camelCaseJson, null, 2)
+    } catch (error) {
+      console.log("error", error)
+      return "Invalid JSON"
+    }
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setOutput(camelCaseJson(input))
+    }, 500)
+
+    return () => clearTimeout(timer)
+  }, [input])
+
   return (
     <main className="flex p-4 gap-4 min-h-screen">
-      <Textarea className="resize-none" placeholder="Paste json here" />
-      <Textarea className="resize-none" readOnly />
+      <Textarea
+        className="resize-none"
+        placeholder="Paste json here"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <Textarea className="resize-none" readOnly value={output} />
     </main>
   )
 }
