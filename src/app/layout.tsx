7210268f2e34a8686400dfb4e82cbc6403f0e473
Marko <drakulic.marko0@gmail.com>
2024-09-08 16:14:53 +0200

    feat: Title update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import Image from "next/image"
 
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Fitness coach app",
+  title: "JSON to Zod transformer",
   description: "",
 }
 
