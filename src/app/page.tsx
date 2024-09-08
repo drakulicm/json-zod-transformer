ee89ceb9e2400bad6d340d51a636fd88dd733a29
Marko <drakulic.marko0@gmail.com>
2024-09-08 12:50:43 +0200

    feat: Setup with text boxes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Textarea } from "@/components/ui/textarea"
+
 const Home = () => {
-  return <main>Hello world</main>
+  return (
+    <main className="flex p-4 gap-4 min-h-screen">
+      <Textarea className="resize-none" placeholder="Paste json here" />
+      <Textarea className="resize-none" readOnly />
+    </main>
+  )
 }
 
 export default Home
